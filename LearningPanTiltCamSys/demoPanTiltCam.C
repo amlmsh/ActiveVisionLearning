c2f1750c1ae1e14984d678a223d43e67c9b42d01
amlmsh <martin.huelse@gmx.net>
2018-09-02 22:12:45 +0200

    adding the data structures for map learning

--- LearningPanTiltCamSys/demoPanTiltCam.C
+++ LearningPanTiltCamSys/demoPanTiltCam.C
@@ -5,11 +5,17 @@
  *      Author: aml
  */
 
+
 #include "PanTiltCam.H"
+#include "CXmlMap.H"
 #include <iostream>
 
 int main(int argc, char *argv[]){
 	PanTiltCamSys::PanTiltCamDegree ptCam;
+
+	CXmlMap map;
+
+	map.init(2,2,1,500);
 
 	unsigned short SOURCE_SERVER_PORT_;
 	char          *SOURCE_SERVER_ADR_;
@@ -22,12 +28,19 @@ int main(int argc, char *argv[]){
 		exit(0);
 	};
 
+	// open connection to pan-tilt system and
 	try{
-
 		ptCam.connect(argv[1], SOURCE_SERVER_ADR_, SOURCE_SERVER_PORT_);
+	}catch(string msg){
+		cout << "Error: terminate: " << msg << endl;
+	}catch(...){
+		cout << "Unknown error: terminate." << endl;
+	}
 
+
+
+	try{
 		ptCam.close();
-
 	}catch(string msg){
 		cout << "Error: terminate: " << msg << endl;
 	}catch(...){
